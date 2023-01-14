4065b3b7f3559c44a107ceaeb13ad95ffc9f414d
GabagoolOverHere <vicmartindev@gmail.com>
2023-01-14 15:39:26 +0100

    Demo files
    Couple of APIs on which palourde has been tested

--- palourde/__init__.py
+++ palourde/__init__.py
@@ -94,21 +94,36 @@ class Collection:
     item: list[CollectionItem]
 
     def __post_init__(self):
-        self.file = pathlib.Path(f"{slugify(self.info.name)}.md")
+        self.file = pathlib.Path("demo").joinpath("Microsoft Graph API").joinpath(f"{slugify(self.info.name)}.md")
         self.file.touch(exist_ok=True)
 
     def to_markdown(self):
         with open(self.file, 'w') as markdown:
-            markdown.write(f"# {self.info.name}\n---\n\n<br>\n\n")
+
+            # REQUEST NAME
+            markdown.write(f"# {self.info.name}\n\n---\n\n<br>\n\n")
             for item in self.item:
+
+                # REQUEST METHOD AND URL
                 if item.request.__dict__.get("url") is not None:
                     markdown.write(
-                        f"### {item.name}\n---\n> Request\n\n```\n{item.request.method} {item.request.url.raw}\n```\n\n"
+                        f"### {item.name}\n\n---\n> Request\n\n```\n{item.request.method} {item.request.url.raw}\n```\n\n"
                     )
+
+                # REQUEST HEADERS AND BODY
                 if item.request.body and item.request.body.raw:
+                    options = item.request.body.options
+                    language = options.raw.language if options is not None else item.request.body.mode
+                    if bool(item.request.header):
+                        for header in item.request.header:
+                            if "application/json" in header.values():
+                                language = "json"
+                                break
                     markdown.write(
-                        f"\n> Body\n\n```{item.request.body.options.raw.language}\n{item.request.body.raw}\n```\n\n"
+                        f"\n> Body\n\n```{language}\n{item.request.body.raw}\n```\n\n"
                     )
+
+                # REQUEST RESPONSE
                 if bool(item.response):
                     markdown.write(
                         f"\n> Response\n\n```{item.response[0].get('_postman_previewlanguage')}\n{item.response[0].get('body')}\n```\n\n"
