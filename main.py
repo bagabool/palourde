66d3a2feddd36900681856d7c90ecb68d3f89b51
GabagoolOverHere <vicmartindev@gmail.com>
2023-01-14 14:32:04 +0100

    Recursion exploring
    Folder structure is now ignored, only requests are parsed

--- main.py
+++ main.py
@@ -5,10 +5,28 @@ import dacite
 
 import palourde
 
-doc = pathlib.Path("assets").joinpath("Connecteur Hub3e - Office 360 - SID.postman_collection.json")
-with open(doc, 'r') as json_coll:
+
+def flatten(root: dict, out: list) -> None:
+    """
+    Flatten the root dict into a list of requests with a recursive exploration
+    :param root: The postman collection
+    :param out: The list of request
+    :return: None
+    """
+    for data in root.get("item"):
+        if data.get("request") is None:
+            flatten(data, out)
+        else:
+            out.append(data)
+
+
+doc = pathlib.Path("assets").joinpath("Hub3e.postman_collection.json")
+with open(doc, 'r+') as json_coll:
     decoded = json.load(json_coll)
+    items = []
+    flatten(decoded, items)
+    # Flatten the requests, ignoring the folder structure
+    decoded["item"] = items
 
 collection = dacite.from_dict(data_class=palourde.Collection, data=decoded)
 collection.to_markdown()
-
--- palourde/__init__.py
+++ palourde/__init__.py
@@ -1,17 +1,91 @@
 import pathlib
-from slugify import slugify
-
-author = "bagabool"
-version = "1.0"
-
 from dataclasses import dataclass
-from palourde.collection_item import CollectionItem
+from typing import Optional
+
+from slugify import slugify
 
 
 @dataclass
-class CollectionInfo:
+class ResponseHeader:
+    key: str
+    value: str
+
+
+@dataclass
+class CollectionInfo: name: str
+
+
+@dataclass
+class BearerToken:
+    key: str
+    value: str
+
+
+@dataclass
+class RequestAuth:
+    type: str
+    bearer: Optional[list[BearerToken]]
+
+
+@dataclass
+class FormData:
+    key: str
+    type: str
+    src: str
+
+
+@dataclass
+class RequestBodyOptionRaw: language: str
+
+
+@dataclass
+class RequestBodyOption: raw: RequestBodyOptionRaw
+
+
+@dataclass
+class RequestBody:
+    mode: str
+    formdata: Optional[list[FormData]]
+    raw: Optional[str]
+    options: Optional[RequestBodyOption]
+
+
+@dataclass
+class RequestQuery:
+    key: str
+    value: str
+
+
+@dataclass
+class RequestUrl:
+    raw: str
+    query: Optional[list[RequestQuery]]
+
+
+@dataclass
+class CollectionItemRequest:
+    auth: Optional[RequestAuth]
+    method: str
+    header: list
+    body: Optional[RequestBody]
+    url: Optional[RequestUrl]
+
+
+@dataclass
+class CollectionItemResponse:
     name: str
-    schema: str
+    originalRequest: CollectionItemRequest
+    status: str
+    code: int
+    _postman_previewlanguage: str
+    body: str
+
+
+@dataclass
+class CollectionItem:
+    name: str
+    request: CollectionItemRequest
+    response: CollectionItemResponse | list
 
 
 @dataclass
@@ -19,17 +93,23 @@ class Collection:
     info: CollectionInfo
     item: list[CollectionItem]
 
+    def __post_init__(self):
+        self.file = pathlib.Path(f"{slugify(self.info.name)}.md")
+        self.file.touch(exist_ok=True)
+
     def to_markdown(self):
-        file = pathlib.Path(f"{slugify(self.info.name)}.md")
-        file.touch(exist_ok=True)
-        with open(file, 'w') as markdown:
-            markdown.write(f"# {self.info.name}\n\n<br>\n\n")
+        with open(self.file, 'w') as markdown:
+            markdown.write(f"# {self.info.name}\n---\n\n<br>\n\n")
             for item in self.item:
-                markdown.write(f"### {item.name}")
-                markdown.write(f"""
-                    ### {item.name}
-                    > Request
-                    ```
-                    {item.request.method} {item.request.url.raw}
-                    ```
-                """)
+                if item.request.__dict__.get("url") is not None:
+                    markdown.write(
+                        f"### {item.name}\n---\n> Request\n\n```\n{item.request.method} {item.request.url.raw}\n```\n\n"
+                    )
+                if item.request.body and item.request.body.raw:
+                    markdown.write(
+                        f"\n> Body\n\n```{item.request.body.options.raw.language}\n{item.request.body.raw}\n```\n\n"
+                    )
+                if bool(item.response):
+                    markdown.write(
+                        f"\n> Response\n\n```{item.response[0].get('_postman_previewlanguage')}\n{item.response[0].get('body')}\n```\n\n"
+                    )
